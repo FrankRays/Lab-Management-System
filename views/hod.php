6c58c8f89cc59655499afa323f8b1affb4d9e930
thomas <thomasthyparampil@gmail.com>
2016-02-22 11:22:38 +0530

    change pwd visual code

--- views/hod.php
+++ views/hod.php
@@ -27,7 +27,7 @@
 		<p style="color:#aa0000;font-size:20px;align:center;">TASKS</p>
 		<hr>
 		<hr>
-		</form>
+		<form method="post">
 		<ul>
 			<hr></hr>
 			<p><input type="submit" name="createuserbtn" value="Create User"></p>
@@ -43,8 +43,63 @@
 			<p><input type="submit" name="viewstock" value="View Stock"></p>
 			<hr></hr>
 		</ul>
+		</form>
 
 	</div>
+	<!-- center panel-->
+	<div class="center" id="center"> 
+		
+	<?php 
+		
+		if(isset($_POST['changepwd']))
+		{
+			echo'
+				<div id="password">
+				<p style="text-align: center; font-size:20px;">&nbsp;CHANGE PASSWORD</p>
+				<hr />
+				<p></p>
+				<table align="center" border="0" cellpadding="1" cellspacing="5" style="width:500px;">
+					<tbody>
+						<tr>
+							<td width=180px align=right>Current Password&nbsp;&nbsp;</td>
+							<td>:</td>
+							<td><input maxlength="30" name="currentchangepassword" size="25" type="password" /></td>
+						</tr>
+						<tr>
+							<td width=180px align=right>New  Password&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+							<td>:</td>
+							<td><input maxlength="30" name="newchangepassword" size="25" type="password" /></td>
+						</tr>
+						<tr>
+							<td width=180px align=right>Re-enter Password</td>
+							<td>:</td>
+							<td><input maxlength="30" name="reenterchangepassword" size="25" type="password" /></td>
+						</tr>
+					</tbody>
+				</table>
+
+				<p>&nbsp;</p>
+
+				<p style="margin-left: 40%;"><input name="savechangepassword" type="button" value="SAVE" />&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<input name="cancelchangepassword" type="button" value="CANCEL" /></p>
+
+				<hr />
+				<table align="center" border="1" cellpadding="1" cellspacing="1" style="height:100px;width:500px;">
+					<tbody>
+						<tr>
+							<td>&nbsp;</td>
+						</tr>
+					</tbody>
+				</table>
+
+				<p>&nbsp;</p>
+
+						<p>&nbsp;</p>
+				</div>
+				';//end of echo
+			
+		}
+	?>
+		</div>
 	<div class=center> area for display</div>
 	<div id=right>some image perhaps?</div>
 	<?php mysqli_close($conn); ?>
