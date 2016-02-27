23c9c37d0fd1dfb5a26aaafc8357fe91d1712316
thomas <thomasthyparampil@gmail.com>
2016-02-28 00:11:49 +0530

    new method

--- views/incharge.php
+++ views/incharge.php
@@ -88,49 +88,7 @@
 		
 		if(isset($_POST['changepwd']))
 		{
-			echo'
-				<div id="password">
-				<p style="text-align: center; font-size:20px;">&nbsp;CHANGE PASSWORD</p>
-				<hr />
-				<p></p>
-				<table align="center" border="0" cellpadding="1" cellspacing="5" style="width:500px;">
-					<tbody>
-						<tr>
-							<td width=180px align=right>Current Password&nbsp;&nbsp;</td>
-							<td>:</td>
-							<td><input maxlength="30" name="currentchangepassword" size="25" type="password" /></td>
-						</tr>
-						<tr>
-							<td width=180px align=right>New  Password&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-							<td>:</td>
-							<td><input maxlength="30" name="newchangepassword" size="25" type="password" /></td>
-						</tr>
-						<tr>
-							<td width=180px align=right>Re-enter Password</td>
-							<td>:</td>
-							<td><input maxlength="30" name="reenterchangepassword" size="25" type="password" /></td>
-						</tr>
-					</tbody>
-				</table>
-
-				<p>&nbsp;</p>
-
-				<p style="margin-left: 40%;"><input name="savechangepassword" type="button" value="SAVE" />&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<input name="cancelchangepassword" type="button" value="CANCEL" /></p>
-
-				<hr />
-				<table align="center" border="1" cellpadding="1" cellspacing="1" style="height:100px;width:500px;">
-					<tbody>
-						<tr>
-							<td>&nbsp;</td>
-						</tr>
-					</tbody>
-				</table>
-
-				<p>&nbsp;</p>
-
-						<p>&nbsp;</p>
-				</div>
-				';//end of echo
+			include 'changepassword.php';
 			
 		}
 		else if(isset($_POST['purchase']))
